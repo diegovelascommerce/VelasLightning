35e410752a9030edb9ea543d099eb8da6f115fcf
Diego Vila <diego@velascommerce.com>
2022-11-11 16:53:39 -0400

    added some more api methods for raspberryblitz

--- VelasLightningAPI/routes.py
+++ VelasLightningAPI/routes.py
@@ -9,15 +9,35 @@ def configure_routes(app, velas):
         """Just a basic route for testing purposes."""
         return "Hello VelasLightning"
 
-    @app.route('/submitBolt11', methods=['POST'])
-    def submitBolt11():
+    @app.route('/get_node_id', methods=['get'])
+    def getNodeId():
+        """ 
+        Return the NodeId of the workit lightning node. 
+        
+        Workit client app will then use the nodeId to attempt to connect to it.
+
+        return:
+            the nodeId of the backend lightning node
+        """
+        return velas.getNodeID()
+
+    @app.route('/create_channel', methods=['post'])
+    def create_channel():
+        """ 
+        Create a channel from nodeId, address and port
+        """
+        data = request.get_json()
+        nodeId = data.get('nodeId')
+        address = data.get('address')
+        port = data.get('port')
+        res = velas.create_channel(nodeId, address, port)
+        return res, 200
+
+    @app.route('/submit_bolt11', methods=['POST'])
+    def submit_bolt11():
         """Passes bolt11 to LAPP."""
         print("submitBolt11")
         data = request.get_json()
         bolt11 = data.get('bolt11')
         velas.payBolt11(bolt11)
         return "Ok", 200
-
-    @app.route('/getNodeId', methods-['get'])
-    def getNodeId():
-        return velas.getNodeID
--- VelasLightningAPI/tests/test_api.py
+++ VelasLightningAPI/tests/test_api.py
@@ -1,3 +1,11 @@
+"""
+These are a list of REST API functions that will be runing on the same machine as the rasperblitz.
+
+These functons are necessary for workit to integrate lightning awards to their app.
+
+
+"""
+
 from urllib import response
 from flask import Flask
 import pytest
@@ -5,6 +13,9 @@ import json
 
 from ..app import configure_routes
 from ..LAPP import Velas
+
+TEST_NODE_ID = "03e347d089c071c27680e26299223e80a740cf3e3fc4b4237fa219bb67121a670b"
+TEST_CHANNEL_ID = "2583303668972126208"
 
 
 @pytest.fixture
@@ -16,15 +27,42 @@ def client():
     return client
 
 
-def test_index(client):
-    response = client.get("/")
-    assert response.get_data() == b"Hello VelasLightning"
-    assert response.status_code == 200
+def test_get_node_id(client):
+    """Retrurn NodeId of lnd so that workit client and setup a peer connection with it."""
+    response = client.get("/get_node_id")
+    assert response.text == TEST_NODE_ID
 
 
-def test_submitBolt11(client):
-    data = {"bolt11": "testBolt11Invoice"}
-    response = client.post('/submitBolt11',
+def test_create_channel(client):
+    """
+    Create outbounded channel with workit app.
+    
+    the workit app will provide the nodeId, address, and port so that we can create
+    an outbounded channel with workit app. 
+    """
+    data = {
+        'nodeId':
+        "02393813695fc7d7bc946ccfa64f65c7d699ac04ccf6b1b5198f1a33c975988e52",
+        'address': "173.70.37.248",
+        'port': 9735
+    }
+    response = client.post('/create_channel',
                            data=json.dumps(data),
                            content_type='application/json')
     assert response.status_code == 200
+    assert response.text == TEST_CHANNEL_ID
+
+
+def test_submit_bolt11(client):
+    """ 
+    this is where workit will submit a bolt11 invoice to be payed automatically.
+
+    the workit app will create the invoice but it the workit backend that will forward it
+    to the REST API for raspberryblitz.
+    """
+    data = {"bolt11": "testBolt11Invoice"}
+    response = client.post('/submit_bolt11',
+                           data=json.dumps(data),
+                           content_type='application/json')
+    assert response.status_code == 200
+    assert response.text == "Ok"
