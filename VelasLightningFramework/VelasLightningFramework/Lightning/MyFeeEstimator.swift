4fe3e32caf691c3897a6b101af3646cc6620c07c
Diego Vila <diego@velascommerce.com>
2023-03-14 14:07:18 -0400

    fixed FeeEstimator

--- VelasLightningFramework/VelasLightningFramework/Lightning/MyFeeEstimator.swift
+++ VelasLightningFramework/VelasLightningFramework/Lightning/MyFeeEstimator.swift
@@ -9,15 +9,18 @@ var feerate_slow = 253
 class MyFeeEstimator: FeeEstimator {
     
     override func getEstSatPer_1000Weight(confirmationTarget: Bindings.ConfirmationTarget) -> UInt32 {
-//        if (confirmationTarget as AnyObject === LDKConfirmationTarget_HighPriority as AnyObject) {
-//            print("LDK/FeeEstimator: \(UInt32(feerate_fast))")
-//            return UInt32(feerate_fast)
-//        }
-//        if (confirmationTarget as AnyObject === LDKConfirmationTarget_Normal as AnyObject) {
-//            print("LDK/FeeEstimator: \(UInt32(feerate_medium))")
-//            return UInt32(feerate_medium)
-//        }
-        return UInt32(feerate_medium)
+        if (confirmationTarget as AnyObject === LDKConfirmationTarget_HighPriority as AnyObject) {
+            print("LDK/FeeEstimator: \(UInt32(feerate_fast))")
+            return UInt32(feerate_fast)
+        }
+        
+        if (confirmationTarget as AnyObject === LDKConfirmationTarget_Normal as AnyObject) {
+            print("LDK/FeeEstimator: \(UInt32(feerate_medium))")
+            return UInt32(feerate_medium)
+        }
+        
+        print("LDK/FeeEstimator: \(UInt32(feerate_slow))")
+        return UInt32(feerate_slow)
     }
     
 }
