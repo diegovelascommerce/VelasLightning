82a75ef28780f91bf1899f1f057b005163b4f80a
Diego Vila <diego@velascommerce.com>
2024-01-01 14:09:05 -0400

    pyright, ruff, debugpy

--- VelasLightningAPI/velas_jwt.py
+++ VelasLightningAPI/velas_jwt.py
@@ -5,15 +5,15 @@ from config import config
 
 def create_jwt(payload: dict) -> str:
     print(f"secret:{config['secret']}")
-    token = jwt.encode(payload, config['secret'], algorithm='HS256')
+    token = jwt.encode(payload, config["secret"], algorithm="HS256")
     return token
 
 
 def verify_jwt(token) -> dict:
-    payload = jwt.decode(token, config['secret'], algorithms=['HS256'])
+    payload = jwt.decode(token, config["secret"], algorithms=["HS256"])
     return payload
 
 
-if __name__ == '__main__':
-    token = create_jwt({'iss': 'velas', 'sub': 'workit'})
+if __name__ == "__main__":
+    token = create_jwt({"iss": "velas", "sub": "workit"})
     print(token)
